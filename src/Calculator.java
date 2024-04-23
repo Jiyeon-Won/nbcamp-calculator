0e2986c135330da7845fef51b158d556a6833955
jiyeon <dnjs229@gamil.com>
2024-04-23 20:26:31 +0900

    Level 2 - 1

---
+++ src/Calculator.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    List<Integer> list = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operation) throws Exception {
+        int result = 0;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
+                result = num1 / num2;
+                break;
+        }
+        list.add(result);
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -12,31 +12,31 @@ public class Main {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char ch = sc.next().charAt(0);
             int result = 0;
-            switch (ch) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        break;
-                    }
-                    result = num1 / num2;
-                    break;
-                case '%':
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        break;
-                    }
-                    result = num1 % num2;
-                    break;
-            }
+//            switch (ch) {
+//                case '+':
+//                    result = num1 + num2;
+//                    break;
+//                case '-':
+//                    result = num1 - num2;
+//                    break;
+//                case '*':
+//                    result = num1 * num2;
+//                    break;
+//                case '/':
+//                    if (num2 == 0) {
+//                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+//                        break;
+//                    }
+//                    result = num1 / num2;
+//                    break;
+//                case '%':
+//                    if (num2 == 0) {
+//                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+//                        break;
+//                    }
+//                    result = num1 % num2;
+//                    break;
+//            }
             list.add(result);
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
