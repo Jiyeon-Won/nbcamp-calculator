9b7a5432637c2470661d9a7e887ef836bfb49dd3
jiyeon <dnjs229@gamil.com>
2024-04-23 12:08:01 +0900

    Level 1 - 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
     }
 }
