4ed1b2d71c110916a13bb031dedd6e8afd5efbd5
jiyeon <dnjs229@gamil.com>
2024-04-23 12:11:50 +0900

    Level 1 - 2

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ public class Main {
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("num1 = " + num1);
-        System.out.println("num2 = " + num2);
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char ch = sc.next().charAt(0);
+
+        System.out.printf("num1 : %d, num2 : %d, ch : %c", num1, num2, ch);
     }
 }
